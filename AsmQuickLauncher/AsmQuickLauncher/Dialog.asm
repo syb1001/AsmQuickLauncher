dfb18dffad4a0f4753233092d8e27058de65e952
syb1001 <syb11@mails.tsinghua.edu.cn>
2014-04-04 16:22:30 +0800

    Add common dialog to get path of a file or a directory

--- AsmQuickLauncher/AsmQuickLauncher/Dialog.asm
+++ AsmQuickLauncher/AsmQuickLauncher/Dialog.asm
@@ -5,10 +5,16 @@ option casemap:none
 include Declaration.inc
 
 .const
+szFileFilter	db		'All Files(*.*)', 0 , '*.*', 0, 0
 szTextTest		db		'≤‚ ‘“ªœ¬', 0
+
+.data
+szCurrentPath	db		MAX_PATH DUP (?)
 
 .code
 _ProcDlgMain PROC uses ebx edi esi hWnd, wMsg, wParam, lParam
+	LOCAL	@ofn:	OPENFILENAME,
+			@bi:	BROWSEINFO
 
 	mov		eax, wMsg
 	.if		eax == WM_CLOSE
@@ -20,7 +26,27 @@ _ProcDlgMain PROC uses ebx edi esi hWnd, wMsg, wParam, lParam
 			.elseif	ax == IDCANCEL
 					invoke	EndDialog, hWnd, NULL
 			.elseif ax == IDC_ChooseFile
-					invoke	MessageBox, hWinMain, addr szTextTest, addr szTextTest, MB_OK
+					invoke	RtlZeroMemory, addr @ofn, sizeof @ofn
+					mov		@ofn.lStructSize, sizeof @ofn
+					push	hWnd
+					pop		@ofn.hwndOwner
+					mov		@ofn.lpstrFilter, offset szFileFilter
+					mov		@ofn.lpstrFile, offset szCurrentPath
+					mov		@ofn.nMaxFile, MAX_PATH 
+					mov		@ofn.Flags, OFN_FILEMUSTEXIST or OFN_PATHMUSTEXIST
+					invoke	GetOpenFileName, addr @ofn
+					.if	eax
+						invoke SetDlgItemText, hWnd, IDC_GesturePath, addr szCurrentPath
+					.endif
+			.elseif ax == IDC_ChooseDirectory
+					invoke	RtlZeroMemory, addr @bi, sizeof @bi
+					push	hWnd
+					pop		@bi.hwndOwner
+					invoke	SHBrowseForFolder, addr @bi
+					.if	eax
+						invoke SHGetPathFromIDList, eax, offset szCurrentPath
+						invoke SetDlgItemText, hWnd, IDC_GesturePath, addr szCurrentPath
+					.endif
 			.endif
 	.else
 			mov		eax, FALSE
