08a1bb116d019e6cdb9c587761608062d7c819c5
FanChuang <zhuangchenfan@gmail.com>
2014-04-05 16:15:28 +0800

    NEW: FILE INPUT & OUTPUT

--- AsmQuickLauncher/AsmQuickLauncher/FileOperation.asm
+++ AsmQuickLauncher/AsmQuickLauncher/FileOperation.asm
@@ -13,66 +13,84 @@ option casemap:none
 include Declaration.inc
 
 .data
-BufSize DWORD MAX_BUF_SIZE
+
 szFileName	BYTE SETTINGS_FILE
 szErrOpenFile BYTE 'Can not open settings.ini', 0
 szErrCreateFile BYTE 'Can not update settings.ini', 0
-fpHandle WORD ?
+
+fpHandle DWORD ?
+
+szFileName1 BYTE 'settings.ini',0
+
+szSpace BYTE ' ', 0
+szCrlf BYTE 13,10, 0
 
 .code 
 
-OutputDword2File PROC uses eax @hFile,
+OutputDword2File PROC,  
 	num: DWORD
-LOCAL @dwBytesWrite: dword,  buf[1024]: byte
-LOCAL space:byte
+	LOCAL @dwBytesWrite, buf[MAX_BUF_SIZE]: byte
 
-	mov BYTE PTR space, 32
-	
 	pushad
-	;invoke ConvertDword2Str, num, ADDR buf 
-	invoke dw2a, num, addr buf
-	invoke WriteFile, @hFile, addr buf, 255, @dwBytesWrite, 0	; output len 
-	invoke WriteFile, @hFile, addr space, 1, @dwBytesWrite, 0	; output a space 
-	popad
+	invoke dw2a, num, addr buf 	; convert DWORD to BYTE 
+	invoke lstrlen, addr buf 	; count the length 
+	mov edx, eax
+	invoke WriteFile, fpHandle, addr buf, edx, addr @dwBytesWrite, 0
+	popad 
 
 	ret 
 OutputDword2File ENDP
 
-OutputByte2File PROC uses eax @hFile,
+OutputByte2File PROC,
 	buf: PTR BYTE
-LOCAL @dwBytesWrite
-LOCAL cr:byte, lf:byte 
-
-	mov BYTE PTR cr, 13
-	mov BYTE PTR lf, 10
+	LOCAL @dwBytesWrite
 
 	pushad
-	invoke WriteFile, @hFile, addr buf, 255, @dwBytesWrite, 0	; output len 
-	
-	invoke WriteFile, @hFile, addr cr, 1, @dwBytesWrite, 0	
-	invoke WriteFile, @hFile, addr lf, 1, @dwBytesWrite, 0	
-	
-	popad
-
+	invoke lstrlen, buf ; count the length 
+	mov edx, eax
+	invoke WriteFile, fpHandle, buf, edx, addr @dwBytesWrite, 0	 
+	popad 
 	ret 
 OutputByte2File ENDP
+
+OutputSpace2File PROC
+	LOCAL @dwBytesWrite
+	pushad
+	invoke lstrlen, addr szSpace ; count the length 
+	mov edx, eax
+	invoke WriteFile, fpHandle, addr szSpace, edx, addr @dwBytesWrite, 0
+	popad 
+	ret 
+OutputSpace2File ENDP
+
+OutputCrlf2File PROC
+	LOCAL @dwBytesWrite
+	pushad
+	invoke lstrlen, addr szCrlf ; count the length 
+	mov edx, eax
+	invoke WriteFile, fpHandle, addr szCrlf, edx, addr @dwBytesWrite, 0
+	popad 
+	ret 
+OutputCrlf2File ENDP
 
 ImportAcitons PROC
 	LOCAL @hFile, @dwBytesRead
 	LOCAL @szReadBuffer: byte
 	LOCAL curStep: DWORD, seqLen: DWORD, seq[MAX_SEQ_LEN]:DWORD,seqIndex: DWORD
 	LOCAL path[1024]:DWORD, pathIndex: DWORD
-	pushad 
 
-	; open settings.ini
+	pushad 
+	;---------------- open settings.ini ------------------------------
 	INVOKE	CreateFile,addr szFileName,GENERIC_READ,FILE_SHARE_READ,0,\
 			OPEN_EXISTING,FILE_ATTRIBUTE_NORMAL,0
 	.if	eax ==	INVALID_HANDLE_VALUE
 		invoke	MessageBox,hWinMain,addr szErrOpenFile,NULL,MB_OK or MB_ICONEXCLAMATION
 		ret
 	.endif
-	mov	@hFile,eax
+	mov	@hFile,eax	; store the fileHandle 
+	;------------------------------------------------------------------
 
+	;------------------------------------------------------------------
 	; load data to actionMap
 	; read a single char each time 
 	xor	eax,eax
@@ -87,33 +105,38 @@ ImportAcitons PROC
 		lea	esi, @szReadBuffer
 		invoke	ReadFile, @hFile, esi, 1, addr @dwBytesRead, 0
 		.break	.if ! @dwBytesRead
-			
-		.if @szReadBuffer == 10	|| @szReadBuffer == 13	; crlf 
+		
+		;------touch a crlf --> input a complete ACTION--------- 	
+		.if @szReadBuffer == 10	|| @szReadBuffer == 13	
 
 			.if seqLen != 0
-
+			;--------add aciton to map ---------
 				lea ebx, path
 				mov edx, pathIndex
 				lea edi, [ebx + edx * TYPE BYTE]
 				mov BYTE PTR [edi], 0 				; path should ends up with 0
-				;INVOKE MessageBox, 0, addr @szReadBuffer, addr path, 0
-
-				INVOKE AddNewAction, addr seq, seqLen, addr path
-
-				mov seqLen, 0 		; clear all for next action
+			
+				INVOKE AddNewAction, addr seq, seqLen, addr path 	; add new action 
+			;-------------------------------------	
+			;--------clear all for next action-------		
+				mov seqLen, 0 		
 				mov curStep, 0
 				mov seqIndex, 0
 				mov pathIndex, 0
+			;----------------------------------------
 			.endif 
-			
-		.elseif @szReadBuffer == 32    ; space = 32
+
+		;----------------- touch a space ---------------
+		.elseif @szReadBuffer == 32    ; touch a space 
 			
 			mov eax, curStep
 			inc eax
 			mov curStep, eax 
 
+		;----------- touch char -------------------------
 		.else
-			.if curStep == 0					; get len
+			;------get len---------
+			.if curStep == 0					
 				
 				movzx eax, @szReadBuffer
 				mov edx, seqLen
@@ -121,6 +144,8 @@ ImportAcitons PROC
 				sub eax, '0'
 				add edx, eax
 				mov seqLen, edx 
+			;------------------------
+			;------get seq-----------
 			.elseif curStep == 1				; get sequence
 				
 				movzx eax, @szReadBuffer
@@ -131,6 +156,8 @@ ImportAcitons PROC
 				mov eax, seqIndex
 				inc eax 
 				mov seqIndex, eax
+			;--------------------------
+			;------get path------------ 
 			.else 								; get path 
 				lea esi, @szReadBuffer			; esi points to src
 
@@ -145,10 +172,22 @@ ImportAcitons PROC
 				mov pathIndex, eax
 								
 			.endif
-
+			;--------------------------
 		.endif
 
 	.endw
+
+	; in case that there is no a crlf in the end 
+	.if seqLen != 0
+
+		lea ebx, path
+		mov edx, pathIndex
+		lea edi, [ebx + edx * TYPE BYTE]
+		mov BYTE PTR [edi], 0 				; path should ends up with 0
+	
+		INVOKE AddNewAction, addr seq, seqLen, addr path 	; add new action 
+
+	.endif 
 
 	INVOKE	CloseHandle, @hFile
 
@@ -159,61 +198,54 @@ ImportAcitons ENDP
 
 
 ExportActions PROC
-	LOCAL @hFile
-	local buf[20]:byte, @dwBytesWrite:dword
-
-	invoke	CreateFile,addr szFileName,GENERIC_WRITE,FILE_SHARE_READ,\
+	local index: dword
+	
+	pushad
+	;---------------- open settings.ini ------------------------------
+	invoke	CreateFile,addr szFileName1,GENERIC_WRITE,FILE_SHARE_READ,\
 			0,CREATE_ALWAYS,FILE_ATTRIBUTE_NORMAL,0
 	.if	eax ==	INVALID_HANDLE_VALUE
 			invoke	MessageBox,hWinMain,addr szErrCreateFile,NULL,MB_OK or MB_ICONEXCLAMATION
 			ret
 	.endif
-	mov	@hFile,eax	
+	mov fpHandle, eax	; store the fileHandle 
+	;------------------------------------------------------------------
 
-   invoke WriteFile, @hFile, addr buf, 255, @dwBytesWrite, 0
 
 	mov ecx, actionLen	; loop counter
-	xor edx, edx 		; index of actionMap
 
 	mov edi, OFFSET actionMap 	; point to the first element of actionMap
+
+	.while ecx > 0
+		
+		invoke OutputDword2File, (ACTION PTR [edi]).len ; output len 
+		invoke OutputSpace2File
+
+		mov ebx, (ACTION PTR [edi]).len
+		lea esi, (ACTION PTR [edi]).seq
+
+		.while ebx > 0 		; output seq 
+			
+			invoke OutputDword2File, [esi]
+
+			add esi, TYPE DWORD
+			dec ebx 
+		.endw 
+		
+		invoke OutputSpace2File
+		invoke OutputByte2File, ADDR (ACTION PTR [edi]).path 	; output action path 
+		invoke OutputCrlf2File
+		
+		add edi, TYPE ACTION 
+		dec ecx
+	.endw 
+
+	INVOKE	CloseHandle, fpHandle
 	
-Output2FileLoop: 	; bug: if actionLen = 0
-	
-	invoke OutputDword2File, @hFile, (ACTION PTR [edi]).len
+	popad
 
-	push ecx
-	mov ecx, (ACTION PTR [edi]).len
-
-	lea esi, (ACTION PTR [edi]).seq
-@@:
-	
-	invoke OutputDword2File, @hFile, [esi]
-
-	add esi, TYPE DWORD
-	loop @B
-
-	pop ecx 
-
-	invoke OutputByte2File, @hFile, ADDR (ACTION PTR [edi]).path
-
-	add edi, TYPE ACTION 
-	loop Output2FileLoop
-
-	INVOKE	CloseHandle, @hFile
-	
 	ret
 
 ExportActions ENDP
-
-ConvertDword2Str PROC,
-	num: DWORD,
-	s: PTR BYTE 
-
-	mov eax, num 
-	mov esi, s
-	invoke dw2a, eax, esi 
-
-	ret 
-ConvertDword2Str ENDP
 
 END
